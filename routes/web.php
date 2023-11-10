d2e535bd416e1a5f867c5db1855e02b3f25f1798
성민 <63378895+dureotkd@users.noreply.github.com>
2023-11-10 10:33:23 +0900

    라우팅 기초 공부 - 01

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,49 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+/**
+ * Web Route 공간
+ */
+// Route::get('/', function () {
+
+//     return view('welcome');
+// });
+
+/**
+ * route arguments 활용
+ * 
+ * '/test/{foo}' (foo 의무)
+ * '/test/{foo?}' (선택적)
+ * 
+ */
+
+# ========== 숫자나영어 3개 필수로.. (pattern 은 모든 foo를 사용하는곳) ==========
+# Route::pattern('foo', '[0-9a-zA-Z]{3}');
+Route::get('/test/{foo}', function ($foo) {
+
+    echo $foo;
+
+    return '<h1>Hello !</h1>';
+});
+# ========== ->where('foo', '[0-9a-zA-Z]{3}'); 는 해당 라우터만 패턴적용 ==========
+
+
+Route::get('/', [
+    'as' => 'home', // 라우터의 별칭 적용
+
+    /**
+     * 접근시 실행 시킬 함수
+     */
+    function () {
+        return '제 이름은 "home" 입니다.';
+    }
+]);
+
+Route::get('/home', function () {
+
+    /**
+     * 별칭 home 루트로 강제 이동
+     */
+    return redirect(route('home'));
 });
